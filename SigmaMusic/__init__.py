137afba39eca24d311c560d3bdceee8c6290ad46
Stargirl-15 <101620596+Stargirl-15@users.noreply.github.com>
2022-05-11 19:47:04 +0530

    Create __init__.py

---
+++ SigmaMusic/__init__.py
@@ -0,0 +1,38 @@
+from SigmaMusic.core.bot import SigmaBot
+from SigmaMusic.core.dir import dirr
+from SigmaMusic.core.git import git
+from SigmaMusic.core.userbot import Userbot
+from SigmaMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = SigmaBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
